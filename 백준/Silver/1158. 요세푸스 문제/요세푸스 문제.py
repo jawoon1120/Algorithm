5e36938ef3a05430c4317b59f5a94259382d5a89
Jawoon Jeong <55796414+jawoon1120@users.noreply.github.com>
2023-01-05 21:25:16 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2188 ms, Memory: 34060 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+n, k = map(int,input().split())
+people = deque([i for i in range(1, n+1)])
+result = []
+
+while people:
+  for _ in range(k-1):
+    people.append(people.popleft())
+
+  result.append(people.popleft())
+
+print(str(result).replace('[','<').replace(']','>'))

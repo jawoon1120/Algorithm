c156c08746e5e93553abdf1dd51236c423cdc662
Jawoon Jeong <55796414+jawoon1120@users.noreply.github.com>
2024-11-13 23:22:12 +0900

    Time: 766 ms (31.59%), Space: 17.4 MB (78.12%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        for index, num in enumerate(nums):
+            
+            exclude_self = nums[:index] + nums[index+1:]
+            if target - num in exclude_self: result.append(index)
+            
+                
+
+        return result
